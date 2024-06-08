33216f05e00cb1b890319411999639a0e259b045
cpm <162544069+cpm-code@users.noreply.github.com>
2024-06-08 02:30:46 +0000

    DV AML Control in Utils

--- xbmc/utils/AMLUtils.cpp
+++ xbmc/utils/AMLUtils.cpp
@@ -30,6 +30,17 @@
 #include <sys/ioctl.h>
 #include <amcodec/codec.h>
 
+#define FLAG_FORCE_DV_LL        (unsigned int)(0x4000)
+#define DOLBY_VISION_LL_DISABLE (unsigned int)(0)
+#define DOLBY_VISION_LL_YUV422  (unsigned int)(1)
+
+#define DOLBY_VISION_FOLLOW_SOURCE     (unsigned int)(1)
+#define DOLBY_VISION_FORCE_OUTPUT_MODE (unsigned int)(2)
+
+#define DOLBY_VISION_OUTPUT_MODE_IPT        (unsigned int)(0)
+#define DOLBY_VISION_OUTPUT_MODE_IPT_TUNNEL (unsigned int)(1)
+#define DOLBY_VISION_OUTPUT_MODE_BYPASS     (unsigned int)(5)
+
 int aml_get_cpufamily_id()
 {
   static int aml_cpufamily_id = -1;
@@ -87,6 +98,21 @@ bool aml_dv_support_ll()
   }
 
   return support_ll;
+}
+
+bool aml_dv_support_std()
+{
+  int support_std = 0;
+  CRegExp regexp;
+  regexp.RegComp("DV_RGB_444_8BIT");
+  std::string valstr;
+  CSysfsPath dv_cap{"/sys/devices/virtual/amhdmitx/amhdmitx0/dv_cap"};
+  if (dv_cap.Exists())
+  {
+    valstr = dv_cap.Get<std::string>().value();
+    support_std = (regexp.RegFind(valstr) >= 0) ? 1 : 0;
+  }
+  return support_std;
 }
 
 bool aml_display_support_3d()
@@ -224,12 +250,130 @@ bool aml_support_dolby_vision()
 bool aml_dolby_vision_enabled()
 {
   static int dv_enabled = -1;
-  bool dv_user_enabled(!CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_DV_DISABLE));
+  bool dv_user_enabled(CServiceBroker::GetSettingsComponent()->GetSettings()
+                       ->GetInt(CSettings::SETTING_COREELEC_AMLOGIC_DV_MODE) != DV_MODE_OFF);
 
   if (dv_enabled == -1)
-    dv_enabled = (!!aml_support_dolby_vision() && !!aml_display_support_dv());
+    dv_enabled = (!!aml_support_dolby_vision());
 
   return ((dv_enabled && !!dv_user_enabled) == 1);
+}
+
+void aml_dv_on(unsigned int mode, bool enable)
+{
+  const auto settings = CServiceBroker::GetSettingsComponent()->GetSettings();
+  enum DV_TYPE dv_type(static_cast<DV_TYPE>(settings->GetInt(CSettings::SETTING_COREELEC_AMLOGIC_DV_TYPE)));
+
+  // Set the Dolby VSVDB parameter to latest value from user.
+  bool dv_dolby_vsvdb_inject(settings->GetBool(CSettings::SETTING_COREELEC_AMLOGIC_DV_VSVDB_INJECT));
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_dolby_vsvdb_inject", dv_dolby_vsvdb_inject ? 1 : 0);
+
+  std::string dv_dolby_vsvdb_payload(settings->GetString(CSettings::SETTING_COREELEC_AMLOGIC_DV_VSVDB));
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_dolby_vsvdb_payload", dv_dolby_vsvdb_payload);
+
+  // Set the Colorimetery to latest value from user.
+  int colorimetry(settings->GetInt(CSettings::SETTING_COREELEC_AMLOGIC_DV_COLORIMETRY_FOR_STD));
+  CSysfsPath("/sys/module/hdmitx20/parameters/dovi_tv_led_bt2020", (colorimetry == DV_COLORIMETRY_BT2020NC) ? 'Y' : 'N');
+  CSysfsPath("/sys/module/hdmitx20/parameters/dovi_tv_led_no_colorimetry", (colorimetry == DV_COLORIMETRY_REMOVE) ? 'Y' : 'N');
+
+  // Set the HDR for LLDV if DV_TYPE_PLAYER_LED_HDR.
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_hdr_for_lldv", (dv_type == DV_TYPE_PLAYER_LED_HDR) ? 'Y' : 'N'); 
+
+  // force player led mode when enabled
+  CSysfsPath dolby_vision_flags{"/sys/module/amdolby_vision/parameters/dolby_vision_flags"};
+  CSysfsPath dolby_vision_ll_policy{"/sys/module/amdolby_vision/parameters/dolby_vision_ll_policy"};
+
+  if (dolby_vision_flags.Exists() && dolby_vision_ll_policy.Exists())
+  {
+    if (dv_type == DV_TYPE_DISPLAY_LED) // Display Led (DV-Std)
+    {
+      dolby_vision_flags.Set(dolby_vision_flags.Get<unsigned int>().value() & ~(FLAG_FORCE_DV_LL));
+      dolby_vision_ll_policy.Set(DOLBY_VISION_LL_DISABLE);
+    }
+    else // Player Led (DV-LL and HDR)
+    {
+      dolby_vision_flags.Set(dolby_vision_flags.Get<unsigned int>().value() | FLAG_FORCE_DV_LL);
+      dolby_vision_ll_policy.Set(DOLBY_VISION_LL_YUV422);
+    }
+  }
+
+  // Convert to non tunnel if not DV_TYPE_DISPLAY_LED.
+  if ((mode == DOLBY_VISION_OUTPUT_MODE_IPT_TUNNEL) && (dv_type != DV_TYPE_DISPLAY_LED)) 
+    mode = DOLBY_VISION_OUTPUT_MODE_IPT;
+
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_mode", mode);
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_policy", DOLBY_VISION_FORCE_OUTPUT_MODE);
+
+  if (enable) aml_dv_enable();
+}
+
+void aml_dv_off(bool disable)
+{
+  // Reset DV Paremters - will do mode change.
+  CSysfsPath("/sys/class/amdolby_vision/debug", "enable_fel 0");
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_mode", DOLBY_VISION_OUTPUT_MODE_BYPASS);
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_policy", DOLBY_VISION_FOLLOW_SOURCE);
+  
+  // If diable wait for mode change and then disable.
+  if (disable) {
+    CSysfsPath dolby_vision_target_mode{"/sys/module/amdolby_vision/parameters/dolby_vision_target_mode"};
+    if (dolby_vision_target_mode.Exists())
+    {
+      std::chrono::time_point<std::chrono::system_clock> now(std::chrono::system_clock::now());
+      while(dolby_vision_target_mode.Get<unsigned int>().value() != DOLBY_VISION_OUTPUT_MODE_BYPASS && 
+            (std::chrono::system_clock::now() - now) < std::chrono::seconds(3))
+        usleep(10000); // wait 10ms
+    }
+    usleep(400000); // TODO: is there a better way? - currently wait 400ms to make sure it has switched over before switching off 
+                    // This is sill an issue even after waiting for target mode. How about check the hdmi no longer outputting VSIF - from debugfs maybe?
+    aml_dv_disable();
+  }
+}
+
+int aml_dv_set_osd_max(int max)
+{
+  // Set the OSD DV graphic max, only set if currently 0 (auto) - i.e. not already set, or want to reset to 0 (auto)
+  CSysfsPath dolby_vision_graphic_max{"/sys/module/amdolby_vision/parameters/dolby_vision_graphic_max"};
+  if (dolby_vision_graphic_max.Exists()) {
+    existing_max = dolby_vision_graphic_max.Get<unsigned int>().value();
+    if (existing_max == 0 || max == 0) dolby_vision_graphic_max.Set(max);
+  }
+  return existing_max;
+}
+
+void aml_dv_enable()
+{
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_enable", "Y");
+}
+
+void aml_dv_disable()
+{
+  CSysfsPath("/sys/module/amdolby_vision/parameters/dolby_vision_enable", "N");
+}
+
+bool aml_is_dv_enable()
+{
+  CSysfsPath dolby_vision_enable{"/sys/module/amdolby_vision/parameters/dolby_vision_enable"};
+  return (dolby_vision_enable.Exists() && StringUtils::EqualsNoCase(dolby_vision_enable.Get<std::string>().value(), "Y"));
+}
+
+void aml_dv_display_trigger()
+{
+  if (aml_is_dv_enable()) {
+    CSysfsPath display_mode{"/sys/class/display/mode"};
+    if (display_mode.Exists()) display_mode.Set(display_mode.Get<std::string>().value());
+  }
+}
+
+void aml_dv_start()
+{
+  const auto settings = CServiceBroker::GetSettingsComponent()->GetSettings();
+  enum DV_MODE dv_mode(static_cast<DV_MODE>(settings->GetInt(CSettings::SETTING_COREELEC_AMLOGIC_DV_MODE)));
+  if (dv_mode == DV_MODE_ON) {
+    aml_dv_set_osd_max(100);
+    aml_dv_off(true);
+    aml_dv_on(DOLBY_VISION_OUTPUT_MODE_IPT_TUNNEL, true);
+  }
 }
 
 bool aml_has_frac_rate_policy()
