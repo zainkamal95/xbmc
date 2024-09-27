2725fff42d9261187e0f455c398a8c52a4264ff2
cpm <162544069+cpm-code@users.noreply.github.com>
2024-09-27 12:55:07 +0800

    DV HDR10+ Add histogram-plus conversion - calculate avg_pq using mean of pq CDF from @doppingkoala

--- xbmc/utils/HDR10PlusConvert.cpp
+++ xbmc/utils/HDR10PlusConvert.cpp
@@ -32,16 +32,21 @@ constexpr std::uint16_t L1_MAX_PQ_MAX_VALUE = 4095;
 constexpr std::uint16_t L1_AVG_PQ_MIN_VALUE = 819;
 
 static double nits_to_pq(double nits) {
-    double y = nits / ST2084_Y_MAX;
-    return std::pow((ST2084_C1 + ST2084_C2 * std::pow(y, ST2084_M1)) / (1.0 + ST2084_C3 * std::pow(y, ST2084_M1)), ST2084_M2);
+  double y = nits / ST2084_Y_MAX;
+  return std::pow((ST2084_C1 + ST2084_C2 * std::pow(y, ST2084_M1)) / (1.0 + ST2084_C3 * std::pow(y, ST2084_M1)), ST2084_M2);
 }
 
-static double peak_brightness_nits(const Hdr10PlusMetadata& meta, const PeakBrightnessSource& source) {
+static uint16_t cast_pq(double nits) {
+  return static_cast<uint16_t>(std::round(nits_to_pq(nits) * 4095.0));
+}
+
+static uint16_t maximum_pq(const Hdr10PlusMetadata& meta, const PeakBrightnessSource& source) {
 
   if (meta.num_windows == 0) return 0;
 
   switch (source) {
-    
+
+    case PeakBrightnessSource::HistogramPlus:
     case PeakBrightnessSource::Histogram: {
 
       const auto& distributions = meta.luminance[0].distribution_maxrgb;
@@ -52,7 +57,7 @@ static double peak_brightness_nits(const Hdr10PlusMetadata& meta, const PeakBrig
                                       return a.percentile < b.percentile;
                                   });
       const auto& max_value = *max_value_it;
-      return static_cast<double>(max_value.percentile) / 10.0; 
+      return cast_pq(static_cast<double>(max_value.percentile) / 10.0);
     }
 
     case PeakBrightnessSource::Histogram99: {
@@ -60,14 +65,14 @@ static double peak_brightness_nits(const Hdr10PlusMetadata& meta, const PeakBrig
       const auto& distributions = meta.luminance[0].distribution_maxrgb;
       if (distributions.empty()) return 0;
 
-      return static_cast<double>(distributions.back().percentile) / 10.0;
+      return cast_pq(static_cast<double>(distributions.back().percentile) / 10.0);
     }
 
     case PeakBrightnessSource::MaxScl: {
 
       const auto& max_scl = meta.luminance[0].maxscl;
       auto max_value = *std::max_element(max_scl, max_scl + sizeof(max_scl) / sizeof(max_scl[0]));
-      return static_cast<double>(max_value) / 10.0;
+      return cast_pq(static_cast<double>(max_value) / 10.0);
     }
 
     case PeakBrightnessSource::MaxSclLuminance: {
@@ -77,14 +82,42 @@ static double peak_brightness_nits(const Hdr10PlusMetadata& meta, const PeakBrig
       double g = static_cast<double>(max_scl[1]);
       double b = static_cast<double>(max_scl[2]);
       double luminance = (0.2627 * r) + (0.678 * g) + (0.0593 * b);
-      return luminance / 10.0;
+      return cast_pq(luminance / 10.0);
 
     }
   }
 
-  CLog::Log(LOGINFO, "CBitstreamConverter::peak_brightness_nits 6");
-
   return 0;
+}
+
+static uint16_t average_pq(const Hdr10PlusMetadata& meta, const PeakBrightnessSource& source) {
+
+  if (meta.num_windows == 0) return 0;
+
+  if ((source == PeakBrightnessSource::HistogramPlus) &&
+      (meta.luminance[0].num_distribution_maxrgb_percentiles == 9) &&
+      (meta.luminance[0].distribution_maxrgb[1].percentage == 5) && 
+      (meta.luminance[0].distribution_maxrgb[2].percentage == 10)) {
+
+    double pq1 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[0].percentile) / 10.0);
+    double pq2 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[3].percentile) / 10.0);
+    double pq3 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[4].percentile) / 10.0);
+    double pq4 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[5].percentile) / 10.0);
+    double pq5 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[6].percentile) / 10.0);
+    double pq6 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[7].percentile) / 10.0);
+    double pq7 = nits_to_pq(static_cast<double>(meta.luminance[0].distribution_maxrgb[8].percentile) / 10.0);
+
+    double mean_pq = (pq1 + pq2) / 2.0 * 0.2400 +
+                     (pq2 + pq3) / 2.0 * 0.2500 +
+                     (pq3 + pq4) / 2.0 * 0.2500 +
+                     (pq4 + pq5) / 2.0 * 0.1500 +
+                     (pq5 + pq6) / 2.0 * 0.0500 +
+                     (pq6 + pq7) / 2.0 * 0.0498;
+
+    return static_cast<uint16_t>(std::round(mean_pq * 4095.0)); 
+  }
+
+  return cast_pq(static_cast<double>(meta.luminance[0].average_maxrgb) / 10.0);
 }
 
 static uint16_t clamp16(uint16_t d, uint16_t min, uint16_t max) {
@@ -104,42 +137,27 @@ std::vector<uint8_t> create_rpu_nalu_for_hdr10plus(
   const uint16_t max_frame_average_light_level) 
 {
 
-  double avg_nits = 0;
-
-  if (meta.num_windows > 0) avg_nits = static_cast<double>(meta.luminance[0].average_maxrgb) / 10.0;
-
-  double max_nits = peak_brightness_nits(meta, peak_source);
-
   uint16_t min_pq = 0;
   if (min_display_mastering_luminance <= 10) {
     min_pq = 7;
   } else if (min_display_mastering_luminance == 50) {
     min_pq = 62;
-  } 
+  }
 
-  uint16_t max_pq = static_cast<uint16_t>(std::round(nits_to_pq(max_nits) * 4095.0));
-  if (max_pq == 0) { 
+  uint16_t max_pq = maximum_pq(meta, peak_source);
+  if (max_pq == 0) {
     switch (max_display_mastering_luminance) {
-      case 1000:
-        max_pq = 3079;
-        break;
-      case 2000:
-        max_pq = 3388;
-        break;
-      case 4000:
-        max_pq = 3696;
-        break;
-      case 10000:
-        max_pq = 4095;
-        break;
-      default:
-        max_pq = 3079;
+      case 1000:  { max_pq = 3079; break; }
+      case 2000:  { max_pq = 3388; break; }
+      case 4000:  { max_pq = 3696; break; }
+      case 10000: { max_pq = 4095; break; }
+      default:    { max_pq = 3079; break; }
     }
   }
-  
-  uint16_t avg_pq = static_cast<uint16_t>(std::round(nits_to_pq(avg_nits) * 4095.0));
 
-  VdrDmData vdr_dm_data = {}; 
+  uint16_t avg_pq = average_pq(meta, peak_source);
+
+  VdrDmData vdr_dm_data = {};
   vdr_dm_data.min_pq = min_pq;
   vdr_dm_data.max_pq = clamp16(max_pq, L1_MAX_PQ_MIN_VALUE, L1_MAX_PQ_MAX_VALUE);
   vdr_dm_data.avg_pq = clamp16(avg_pq, L1_AVG_PQ_MIN_VALUE, (vdr_dm_data.max_pq - 1));
@@ -148,25 +166,25 @@ std::vector<uint8_t> create_rpu_nalu_for_hdr10plus(
   vdr_dm_data.max_content_light_level = max_content_light_level;
   vdr_dm_data.max_frame_average_light_level = max_frame_average_light_level;
 
-  if ((last_vdr_dm_data.min_pq != vdr_dm_data.min_pq) || 
+  if ((last_vdr_dm_data.min_pq != vdr_dm_data.min_pq) ||
       (last_vdr_dm_data.max_pq != vdr_dm_data.max_pq) ||
-      (last_vdr_dm_data.avg_pq != vdr_dm_data.avg_pq) || 
-      (last_vdr_dm_data.max_display_mastering_luminance != vdr_dm_data.max_display_mastering_luminance) || 
-      (last_vdr_dm_data.min_display_mastering_luminance != vdr_dm_data.min_display_mastering_luminance) || 
-      (last_vdr_dm_data.max_content_light_level != vdr_dm_data.max_content_light_level) || 
+      (last_vdr_dm_data.avg_pq != vdr_dm_data.avg_pq) ||
+      (last_vdr_dm_data.max_display_mastering_luminance != vdr_dm_data.max_display_mastering_luminance) ||
+      (last_vdr_dm_data.min_display_mastering_luminance != vdr_dm_data.min_display_mastering_luminance) ||
+      (last_vdr_dm_data.max_content_light_level != vdr_dm_data.max_content_light_level) ||
       (last_vdr_dm_data.max_frame_average_light_level != vdr_dm_data.max_frame_average_light_level)) {
 
     last_rpu = create_rpu_nalu(vdr_dm_data);
     last_vdr_dm_data = vdr_dm_data;
 
-    CLog::Log(LOGINFO, "HDR10PlusConvert::create_rpu_nalu_for_hdr10plus min_pq [{}] max_pq [{}] avg_pq [{}] mdml max [{}] mdml min [{}] cll [{}] fall [{}]", 
-      vdr_dm_data.min_pq, 
-      vdr_dm_data.max_pq, 
-      vdr_dm_data.avg_pq, 
-      vdr_dm_data.max_display_mastering_luminance, 
-      vdr_dm_data.min_display_mastering_luminance, 
-      vdr_dm_data.max_content_light_level, 
-      vdr_dm_data.max_frame_average_light_level);  
+    CLog::Log(LOGINFO, "HDR10PlusConvert::create_rpu_nalu_for_hdr10plus min_pq [{}] max_pq [{}] avg_pq [{}] mdml max [{}] mdml min [{}] cll [{}] fall [{}]",
+      vdr_dm_data.min_pq,
+      vdr_dm_data.max_pq,
+      vdr_dm_data.avg_pq,
+      vdr_dm_data.max_display_mastering_luminance,
+      vdr_dm_data.min_display_mastering_luminance,
+      vdr_dm_data.max_content_light_level,
+      vdr_dm_data.max_frame_average_light_level);
   }
 
   return last_rpu;
