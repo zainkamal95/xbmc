c8d2e071c9c88f6f428100ef8671424d10a7bcb2
cpm <162544069+cpm-code@users.noreply.github.com>
2024-11-05 10:10:12 +0800

    AgedMap - Add findOrLatest method

--- xbmc/utils/AgedMap.h
+++ xbmc/utils/AgedMap.h
@@ -3,11 +3,14 @@
 #include <unordered_map>
 #include <queue>
 
+// TODO: Make this thread safe, probably not a concern for now.
+
 template<typename K, typename V, size_t MaxSize = 512>
 class AgedMap {
 public:
   void insert(K key, V value);
   auto find(const K& key) const { return map.find(key); }
+  auto findOrLatest(const K& key) const;
   auto end() const { return map.end(); }
   void erase(const K& key);
 
@@ -30,4 +33,18 @@ void AgedMap<K, V, MaxSize>::insert(K key, V value) {
 template<typename K, typename V, size_t MaxSize>
 void AgedMap<K, V, MaxSize>::erase(const K& key) {
   map.erase(key);
+}
+
+template<typename K, typename V, size_t MaxSize>
+auto AgedMap<K, V, MaxSize>::findOrLatest(const K& key) const {
+  auto it = map.find(key);
+  if (it != map.end()) {
+    return it;
+  } else if (!ages.empty()) {
+    // Get the most recently inserted key
+    K latestKey = ages.back();
+    return map.find(latestKey);
+  } else {
+    return map.end();
+  }
 }
