ebc5df4c1aa05d4d7722777664cabf90d9a1360d
cpm <162544069+cpm-code@users.noreply.github.com>
2024-12-25 15:50:20 +0000

    RenderManger: Remove UpdateRenderBuffers not required for Amlogic

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1060,7 +1060,6 @@ bool CRenderManager::AddVideoPicture(const VideoPicture& picture, volatile std::
   m.pts = picture.pts;
   m_queued.push_back(m_free.front());
   m_free.pop_front();
-  m_playerPort->UpdateRenderBuffers(m_queued.size(), m_discard.size(), m_free.size());
 
   // signal to any waiters to check state
   if (m_presentstep == PRESENT_IDLE)
@@ -1285,7 +1284,6 @@ void CRenderManager::PrepareNextRender()
     m_presentpts = m_Queue[idx].pts - m_displayLatency;
     m_presentevent.notifyAll();
 
-    m_playerPort->UpdateRenderBuffers(m_queued.size(), m_discard.size(), m_free.size());
   }
   else if (!combined && renderPts > (nextFramePts - frametime))
   {
