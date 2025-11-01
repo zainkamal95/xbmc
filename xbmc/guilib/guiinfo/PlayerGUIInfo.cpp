badda36d4f5eeac921f1d46dc405c2f4ca387156
zainkamal95 <130873639+zainkamal95@users.noreply.github.com>
2025-11-01 13:53:15 +0200

    Fix redundant return statement in PlayerGUIInfo.cpp

--- xbmc/guilib/guiinfo/PlayerGUIInfo.cpp
+++ xbmc/guilib/guiinfo/PlayerGUIInfo.cpp
@@ -397,9 +397,9 @@ bool CPlayerGUIInfo::GetLabel(std::string& value, const CFileItem *item, int con
       return true;
     case PLAYER_PROCESS_VIDEO_LIVE_BIT_RATE:
       value = std::to_string(CServiceBroker::GetDataCacheCore().GetVideoLiveBitRate() / 1024 / 1024);
+      return true;
     case PLAYER_PROCESS_AUDIO_LIVE_BIT_RATE:
       value = std::to_string(CServiceBroker::GetDataCacheCore().GetAudioLiveBitRate() / 1024);
-      return true;
       return true;
     case PLAYER_PROCESS_AML_PIXELFORMAT:
       value = GetAMLConfigInfo("Colour depth") + ", " + GetAMLConfigInfo("Colourspace");
