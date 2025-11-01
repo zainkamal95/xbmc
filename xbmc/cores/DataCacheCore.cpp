cab65c91f9bfe5b191502d2bf1be31700a9b0569
zainkamal95 <130873639+zainkamal95@users.noreply.github.com>
2025-11-01 13:26:33 +0200

    Add methods to set and get video live bitrate

--- xbmc/cores/DataCacheCore.cpp
+++ xbmc/cores/DataCacheCore.cpp
@@ -168,6 +168,7 @@ int CDataCacheCore::GetVideoHeight()
 
 void CDataCacheCore::SetVideoLiveBitRate(double bitRate)
 {
+  
   std::unique_lock<CCriticalSection> lock(m_videoPlayerSection);
 
   m_playerVideoInfo.liveBitRate = bitRate;
@@ -175,6 +176,7 @@ void CDataCacheCore::SetVideoLiveBitRate(double bitRate)
 
 double CDataCacheCore::GetVideoLiveBitRate()
 {
+  
   std::unique_lock<CCriticalSection> lock(m_videoPlayerSection);
 
   return m_playerVideoInfo.liveBitRate;
